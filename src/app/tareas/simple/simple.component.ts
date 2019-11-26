23c0dc64f2e62c75081eb318059199b2e01d6cf6
kaxhy <antonio.cachaza@gmail.com>
2019-11-26 16:33:47 +0100

    tareas

--- src/app/tareas/simple/simple.component.ts
+++ src/app/tareas/simple/simple.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TareaModel } from 'src/app/models/tarea.model';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'aub-simple',
@@ -10,14 +12,22 @@ export class SimpleComponent implements OnInit {
   tareas: Array<TareaModel>;
   newTarea: TareaModel;
   // tarea:Tareaif;
+  @ViewChild('confirmar', {static: true}) confirmar: ElementRef;
+  storeName: string;
+  papelera: IconDefinition ;
 
   constructor() {
-    this.tareas = [] as TareaModel[];
+    // this.tareas = [] as TareaModel[];
     this.newTarea = new TareaModel();
+    this.papelera = faTrashAlt;
+    this.storeName = 'Tareas';
     // this.tarea = {} as Tareaif;
   }
 
   ngOnInit() {
+
+    this.tareas = ( JSON.parse(localStorage.getItem(this.storeName)) || [] ) as TareaModel[];
+
   }
 
   addTareas(ev: Event): void {
@@ -26,7 +36,32 @@ export class SimpleComponent implements OnInit {
     }
     this.tareas.push({... this.newTarea});
     this.newTarea =  new TareaModel();
-    console.log(this.tareas);
+    this.actualizarStore();
   }
 
+  onConfirmTareas(): void {
+    this.confirmar.nativeElement.showModal();
+  }
+
+  onDeleteTareas(confirma): void {
+    if(confirma){
+      this.tareas = [] as TareaModel[];
+      this.actualizarStore();
+    }
+    this.confirmar.nativeElement.close();
+  }
+
+  onChange(tarea: TareaModel ): void{
+    console.log(tarea);
+    this.actualizarStore();
+  }
+
+  onRemoveTarea(i: number): void {
+   this.tareas.splice(i, 1);
+   this.actualizarStore();
+  }
+
+  private actualizarStore(): void {
+    localStorage.setItem(this.storeName,JSON.stringify(this.tareas));
+  }
 }
--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -7,12 +7,13 @@ import { MenuComponent } from './menu/menu.component';
 import { SimpleComponent } from './simple/simple.component';
 import { PlusComponent } from './plus/plus.component';
 import { FormsModule } from '@angular/forms';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [MainComponent, MenuComponent, SimpleComponent, PlusComponent],
   imports: [
     FormsModule,
+    FontAwesomeModule,
     CommonModule,
     TareasRoutingModule
   ],
