087857b6365a477b5bcbff5ea043ea5aeaf962c8
kaxhy <antonio.cachaza@gmail.com>
2019-11-28 17:49:30 +0100

    to valirable

--- src/app/cursos/from-md/from-md.component.ts
+++ src/app/cursos/from-md/from-md.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { MaestroDatos } from 'src/app/models/alumno-model';
+import { MaestroDatos, AlumnoModel } from 'src/app/models/alumno-model';
 import { TURNOS, CURSOS } from 'src/app/models/cursos.data';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PasswordValidator } from 'src/app/shared/validators';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'aub-from-md',
@@ -14,13 +15,14 @@ export class FromMdComponent implements OnInit {
   turnos: Array<MaestroDatos>;
   cursos: Array<MaestroDatos>;
   formMd: FormGroup;
-
+  alumno: AlumnoModel;
   constructor(private formBuilder: FormBuilder) {
 
     this.turnos = TURNOS;
     this.cursos = CURSOS;
 
     this.iniForm();
+    
 
   }
 
@@ -46,6 +48,19 @@ export class FromMdComponent implements OnInit {
       }
     );
     console.dir(this.formMd);
+
+  }
+
+  onSubmit(ev: Event): void {
+    this.alumno = this.formMd.value;
+    /*
+    this.alumno= new AlumnoModel(
+      this.formMd.get('nombre') +''+ this.formMd.get('apellidos'),
+      ...
+    );*/
+    console.log(this.alumno);
+    this.formMd.reset();
+
 
   }
 
